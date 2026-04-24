2852a80311b54d64d09b773a67380be78984e8a0
c25108ds <c25108ds@aiit.ac.jp>
2026-04-24 00:41:04 +0000

    Remove git commands from script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate # これが本番環境でテーブルを作る命令です
-git add -A
-git commit -m "Force redeploy"
-git push
+bundle exec rails db:migrate
