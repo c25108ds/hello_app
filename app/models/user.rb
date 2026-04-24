2fef6d0f9ed24b2ee9e7912c078d999eed94dcf4
c25108ds <c25108ds@aiit.ac.jp>
2026-04-24 00:29:33 +0000

    Fix typo in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  has_many :microposts # 追記
+  has_many :microposts
 end
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate # これが本番環境でテーブルを作る命令です
+git add -A
+git commit -m "Force redeploy"
+git push
